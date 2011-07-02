0ee5208c0980f86e6f96534bd3b23a331b75ff0c
Pieter van Ginkel <pvginkel@gmail.com>
2011-07-02 11:51:12 +0200

    * Persist known hosts.

--- SharpGit/SshSessionFactory.cs
+++ SharpGit/SshSessionFactory.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NGit.Transport;
+using System.IO;
 
 namespace SharpGit
 {
     internal class SshSessionFactory : JschConfigSessionFactory
     {
+        private static readonly object _syncLock = new object();
+        private static string _knownHostsFilename;
+
         public static SshSessionFactory Instance = new SshSessionFactory();
 
         protected override NSch.JSch GetJSch(OpenSshConfig.Host hc, NGit.Util.FS fs)
@@ -19,7 +23,39 @@ namespace SharpGit
                 jsch.AddIdentity(certificate.Path);
             }
 
+            // Set the known hosts file.
+
+            jsch.SetKnownHosts(GetKnownHostsFilename());
+
             return jsch;
+        }
+
+        private string GetKnownHostsFilename()
+        {
+            lock (_syncLock)
+            {
+                if (_knownHostsFilename == null)
+                {
+                    string userHome = Environment.GetFolderPath(
+                        Environment.SpecialFolder.UserProfile
+                    );
+
+                    string sshFolder = Path.Combine(userHome, ".ssh");
+
+                    if (!Directory.Exists(sshFolder))
+                    {
+                        DirectoryInfo directoryInfo = Directory.CreateDirectory(sshFolder);
+                        directoryInfo.Attributes |= FileAttributes.Hidden;
+                    }
+
+                    _knownHostsFilename = Path.Combine(sshFolder, "known_hosts");
+
+                    if (!File.Exists(_knownHostsFilename))
+                        File.WriteAllBytes(_knownHostsFilename, new byte[0]);
+                }
+
+                return _knownHostsFilename;
+            }
         }
 
         protected override void Configure(OpenSshConfig.Host hc, NSch.Session session)
