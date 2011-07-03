81fb935ee2f9060f4f041a993050b6a0603a8fad
Pieter van Ginkel <pvginkel@gmail.com>
2011-07-03 07:05:10 +0200

    * Updated design of settings pages.

--- VisualGit.UI/OptionsPages/ToolArgumentDialog.Designer.cs
+++ VisualGit.UI/OptionsPages/ToolArgumentDialog.Designer.cs
@@ -16,43 +16,47 @@ namespace VisualGit.UI.OptionsPages
             this.button1 = new System.Windows.Forms.Button();
             this.textBox = new System.Windows.Forms.RichTextBox();
             this.macroView = new VisualGit.UI.VSSelectionControls.SmartListView();
-            this.nameColumn = new System.Windows.Forms.ColumnHeader();
-            this.valueColumn = new System.Windows.Forms.ColumnHeader();
+            this.nameColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.valueColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // cancelButton
             // 
-            this.cancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.cancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancelButton.Location = new System.Drawing.Point(540, 208);
+            this.cancelButton.Location = new System.Drawing.Point(453, 216);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(75, 23);
-            this.cancelButton.TabIndex = 6;
+            this.cancelButton.TabIndex = 3;
             this.cancelButton.Text = "Cancel";
             // 
             // okButton
             // 
-            this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.okButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.okButton.Location = new System.Drawing.Point(459, 208);
+            this.okButton.Location = new System.Drawing.Point(372, 216);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
-            this.okButton.TabIndex = 5;
+            this.okButton.TabIndex = 2;
             this.okButton.Text = "OK";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 15);
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(57, 35);
             this.label1.TabIndex = 0;
             this.label1.Text = "&Command:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 48);
+            this.tableLayoutPanel1.SetColumnSpan(this.label2, 2);
+            this.label2.Location = new System.Drawing.Point(3, 35);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(45, 13);
             this.label2.TabIndex = 2;
@@ -60,43 +64,41 @@ namespace VisualGit.UI.OptionsPages
             // 
             // button1
             // 
-            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button1.Location = new System.Drawing.Point(12, 208);
+            this.button1.Location = new System.Drawing.Point(3, 216);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 4;
+            this.button1.TabIndex = 1;
             this.button1.Text = "I&nsert";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox
             // 
-            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textBox.Location = new System.Drawing.Point(72, 12);
+            this.textBox.Location = new System.Drawing.Point(66, 3);
             this.textBox.Multiline = false;
             this.textBox.Name = "textBox";
             this.textBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
-            this.textBox.Size = new System.Drawing.Size(543, 29);
+            this.textBox.Size = new System.Drawing.Size(462, 29);
             this.textBox.TabIndex = 1;
             this.textBox.Text = "";
             // 
             // macroView
             // 
-            this.macroView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
             this.macroView.AutoSizeLastColumn = true;
             this.macroView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.nameColumn,
             this.valueColumn});
+            this.tableLayoutPanel2.SetColumnSpan(this.macroView, 2);
+            this.macroView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.macroView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.macroView.Location = new System.Drawing.Point(12, 65);
+            this.macroView.Location = new System.Drawing.Point(3, 3);
             this.macroView.MultiSelect = false;
             this.macroView.Name = "macroView";
-            this.macroView.Size = new System.Drawing.Size(430, 137);
-            this.macroView.TabIndex = 3;
+            this.macroView.Size = new System.Drawing.Size(363, 207);
+            this.macroView.TabIndex = 0;
             this.macroView.DoubleClick += new System.EventHandler(this.macroView_DoubleClick);
             // 
             // nameColumn
@@ -109,24 +111,63 @@ namespace VisualGit.UI.OptionsPages
             this.valueColumn.Text = "Value";
             this.valueColumn.Width = 220;
             // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.textBox, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(531, 290);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.AutoSize = true;
+            this.tableLayoutPanel2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel1.SetColumnSpan(this.tableLayoutPanel2, 2);
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel2.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.macroView, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cancelButton, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.okButton, 2, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(0, 48);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(531, 242);
+            this.tableLayoutPanel2.TabIndex = 3;
+            // 
             // ToolArgumentDialog
             // 
             this.AcceptButton = this.okButton;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.cancelButton;
-            this.ClientSize = new System.Drawing.Size(627, 243);
-            this.Controls.Add(this.textBox);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.macroView);
-            this.Controls.Add(this.cancelButton);
-            this.Controls.Add(this.okButton);
+            this.ClientSize = new System.Drawing.Size(547, 306);
+            this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ToolArgumentDialog";
+            this.Padding = new System.Windows.Forms.Padding(8);
             this.Text = "Command Editor";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -139,5 +180,7 @@ namespace VisualGit.UI.OptionsPages
         private Label label2;
         private Button button1;
         private RichTextBox textBox;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
     }
 }
--- VisualGit.UI/OptionsPages/UserToolSettingsControl.Designer.cs
+++ VisualGit.UI/OptionsPages/UserToolSettingsControl.Designer.cs
@@ -28,95 +28,105 @@ namespace VisualGit.UI.OptionsPages
         /// </summary>
         private void InitializeComponent()
         {
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserToolSettingsControl));
-			this.diffExeBox = new System.Windows.Forms.ComboBox();
-			this.label1 = new System.Windows.Forms.Label();
-			this.diffBrowseBtn = new System.Windows.Forms.Button();
-			this.label2 = new System.Windows.Forms.Label();
-			this.mergeExeBox = new System.Windows.Forms.ComboBox();
-			this.mergeBrowseBtn = new System.Windows.Forms.Button();
-			this.patchBrowseBtn = new System.Windows.Forms.Button();
-			this.patchExeBox = new System.Windows.Forms.ComboBox();
-			this.label3 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// diffExeBox
-			// 
-			resources.ApplyResources(this.diffExeBox, "diffExeBox");
-			this.diffExeBox.DisplayMember = "DisplayName";
-			this.diffExeBox.Name = "diffExeBox";
-			this.diffExeBox.SelectionChangeCommitted += new System.EventHandler(this.tool_selectionCommitted);
-			this.diffExeBox.TextChanged += new System.EventHandler(this.diffExeBox_TextChanged);
-			// 
-			// label1
-			// 
-			resources.ApplyResources(this.label1, "label1");
-			this.label1.Name = "label1";
-			// 
-			// diffBrowseBtn
-			// 
-			resources.ApplyResources(this.diffBrowseBtn, "diffBrowseBtn");
-			this.diffBrowseBtn.Name = "diffBrowseBtn";
-			this.diffBrowseBtn.UseVisualStyleBackColor = true;
-			this.diffBrowseBtn.Click += new System.EventHandler(this.diffBrowseBtn_Click);
-			// 
-			// label2
-			// 
-			resources.ApplyResources(this.label2, "label2");
-			this.label2.Name = "label2";
-			// 
-			// mergeExeBox
-			// 
-			resources.ApplyResources(this.mergeExeBox, "mergeExeBox");
-			this.mergeExeBox.DisplayMember = "DisplayName";
-			this.mergeExeBox.Name = "mergeExeBox";
-			this.mergeExeBox.ValueMember = "ToolTemplate";
-			this.mergeExeBox.SelectionChangeCommitted += new System.EventHandler(this.tool_selectionCommitted);
-			// 
-			// mergeBrowseBtn
-			// 
-			resources.ApplyResources(this.mergeBrowseBtn, "mergeBrowseBtn");
-			this.mergeBrowseBtn.Name = "mergeBrowseBtn";
-			this.mergeBrowseBtn.UseVisualStyleBackColor = true;
-			this.mergeBrowseBtn.Click += new System.EventHandler(this.mergeBrowseBtn_Click);
-			// 
-			// patchBrowseBtn
-			// 
-			resources.ApplyResources(this.patchBrowseBtn, "patchBrowseBtn");
-			this.patchBrowseBtn.Name = "patchBrowseBtn";
-			this.patchBrowseBtn.UseVisualStyleBackColor = true;
-			this.patchBrowseBtn.Click += new System.EventHandler(this.patchBrowseBtn_Click);
-			// 
-			// patchExeBox
-			// 
-			resources.ApplyResources(this.patchExeBox, "patchExeBox");
-			this.patchExeBox.DisplayMember = "DisplayName";
-			this.patchExeBox.Name = "patchExeBox";
-			this.patchExeBox.ValueMember = "ToolTemplate";
-			this.patchExeBox.SelectionChangeCommitted += new System.EventHandler(this.tool_selectionCommitted);
-			this.patchExeBox.SelectedIndexChanged += new System.EventHandler(this.tool_selectionCommitted);
-			// 
-			// label3
-			// 
-			resources.ApplyResources(this.label3, "label3");
-			this.label3.Name = "label3";
-			// 
-			// UserToolSettingsControl
-			// 
-			resources.ApplyResources(this, "$this");
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.diffExeBox);
-			this.Controls.Add(this.diffBrowseBtn);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.mergeExeBox);
-			this.Controls.Add(this.mergeBrowseBtn);
-			this.Controls.Add(this.label3);
-			this.Controls.Add(this.patchExeBox);
-			this.Controls.Add(this.patchBrowseBtn);
-			this.Name = "UserToolSettingsControl";
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserToolSettingsControl));
+            this.diffExeBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.diffBrowseBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mergeExeBox = new System.Windows.Forms.ComboBox();
+            this.mergeBrowseBtn = new System.Windows.Forms.Button();
+            this.patchBrowseBtn = new System.Windows.Forms.Button();
+            this.patchExeBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // diffExeBox
+            // 
+            resources.ApplyResources(this.diffExeBox, "diffExeBox");
+            this.diffExeBox.DisplayMember = "DisplayName";
+            this.diffExeBox.Name = "diffExeBox";
+            this.diffExeBox.SelectionChangeCommitted += new System.EventHandler(this.tool_selectionCommitted);
+            this.diffExeBox.TextChanged += new System.EventHandler(this.diffExeBox_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // diffBrowseBtn
+            // 
+            resources.ApplyResources(this.diffBrowseBtn, "diffBrowseBtn");
+            this.diffBrowseBtn.Name = "diffBrowseBtn";
+            this.diffBrowseBtn.UseVisualStyleBackColor = true;
+            this.diffBrowseBtn.Click += new System.EventHandler(this.diffBrowseBtn_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // mergeExeBox
+            // 
+            resources.ApplyResources(this.mergeExeBox, "mergeExeBox");
+            this.mergeExeBox.DisplayMember = "DisplayName";
+            this.mergeExeBox.Name = "mergeExeBox";
+            this.mergeExeBox.ValueMember = "ToolTemplate";
+            this.mergeExeBox.SelectionChangeCommitted += new System.EventHandler(this.tool_selectionCommitted);
+            // 
+            // mergeBrowseBtn
+            // 
+            resources.ApplyResources(this.mergeBrowseBtn, "mergeBrowseBtn");
+            this.mergeBrowseBtn.Name = "mergeBrowseBtn";
+            this.mergeBrowseBtn.UseVisualStyleBackColor = true;
+            this.mergeBrowseBtn.Click += new System.EventHandler(this.mergeBrowseBtn_Click);
+            // 
+            // patchBrowseBtn
+            // 
+            resources.ApplyResources(this.patchBrowseBtn, "patchBrowseBtn");
+            this.patchBrowseBtn.Name = "patchBrowseBtn";
+            this.patchBrowseBtn.UseVisualStyleBackColor = true;
+            this.patchBrowseBtn.Click += new System.EventHandler(this.patchBrowseBtn_Click);
+            // 
+            // patchExeBox
+            // 
+            resources.ApplyResources(this.patchExeBox, "patchExeBox");
+            this.patchExeBox.DisplayMember = "DisplayName";
+            this.patchExeBox.Name = "patchExeBox";
+            this.patchExeBox.ValueMember = "ToolTemplate";
+            this.patchExeBox.SelectedIndexChanged += new System.EventHandler(this.tool_selectionCommitted);
+            this.patchExeBox.SelectionChangeCommitted += new System.EventHandler(this.tool_selectionCommitted);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.patchBrowseBtn, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.patchExeBox, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.mergeBrowseBtn, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.mergeExeBox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.diffBrowseBtn, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.diffExeBox, 0, 1);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // UserToolSettingsControl
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "UserToolSettingsControl";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -131,5 +141,6 @@ namespace VisualGit.UI.OptionsPages
         private System.Windows.Forms.Button patchBrowseBtn;
         private System.Windows.Forms.ComboBox patchExeBox;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
